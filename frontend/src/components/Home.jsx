d251ffba318438bfd2d20088c4042b0f39726ae7
Humair Abbas Khan <humairabbas923@gmail.com>
2025-02-16 17:19:14 +0500

    updated home

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -7,6 +7,7 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import { Link } from "react-router-dom";
 import { BACKEND_URL } from "../utils/utils";
+
 const Home = () => {
   const [courses, setCourses] = useState([]);
   const [error, setError] = useState(null);
@@ -44,7 +45,7 @@ const Home = () => {
         },
       },
       {
-        breakpoint: 600,
+        breakpoint: 768,
         settings: {
           slidesToShow: 2,
           slidesToScroll: 1,
@@ -64,14 +65,14 @@ const Home = () => {
     <div className="bg-gradient-to-r from-black to-blue-950 min-h-screen text-white">
       <Header />
       <div className="flex flex-col items-center justify-center text-center px-6">
-        <h1 className="text-4xl font-bold text-orange-500 mt-4 mb-3">
+        <h1 className="text-4xl md:text-3xl sm:text-2xl font-bold text-orange-500 mt-4 mb-3">
           LearnSphere
         </h1>
-        <h2 className="text-lg max-w-2xl leading-relaxed">
+        <h2 className="text-lg md:text-base sm:text-sm max-w-2xl leading-relaxed">
           A dynamic and engaging platform offering high-quality courses to help
           you master new skills.
         </h2>
-        <div className=" space-x-4 my-8">
+        <div className="space-x-4 my-8">
           <Link
             to={"/courses"}
             className="bg-orange-500 hover:bg-orange-600 px-5 py-3 rounded-lg text-white font-semibold transition"
@@ -89,27 +90,29 @@ const Home = () => {
 
       <section className="my-7 max-w-6xl mx-auto">
         {error ? (
-          <p className="text-red-500 text-center">{error}</p>
+          <p className="text-red-500 text-center text-sm md:text-base">
+            {error}
+          </p>
         ) : (
           <Slider {...settings}>
             {courses.map((course) => (
               <div key={course._id} className="p-1">
-                <div className="rounded-lg  p-2 text-center">
-                  <div className="h-24 flex justify-center items-center overflow-hidden hover:scale-110 transition">
+                <div className="rounded-lg p-2 text-center">
+                  <div className="h-24 sm:h-20 flex justify-center items-center overflow-hidden hover:scale-110 transition">
                     <img
                       src={course.image.url}
                       alt={course.title}
-                      className="w-4/5 h-4/7 object-cover rounded-xl"
+                      className="w-4/5 sm:w-full h-4/7 object-cover rounded-xl"
                     />
                   </div>
 
                   <div className="mt-4">
-                    <h2 className="text-lg font-semibold mb-2">
+                    <h2 className="text-lg md:text-base sm:text-sm font-semibold mb-2">
                       {course.title}
                     </h2>
                     <Link
                       to={`/buy/${course._id}`}
-                      className=" bg-orange-500 hover:bg-orange-600 px-2 py-2 rounded-lg text-white font-semibold transition"
+                      className="bg-orange-500 hover:bg-orange-600 px-2 py-2 rounded-lg text-white font-semibold transition"
                     >
                       Enroll Now
                     </Link>
