edde9609e6b7f1e908242763a9967104db668203
Humair Abbas Khan <humairabbas923@gmail.com>
2025-02-16 14:46:40 +0500

    added utils.js

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL,
+    origin: process.env.CLIENT_URL,
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
